810b2f0e5eb0a955738a2b435b75b32bf78873cd
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-27 08:19:29 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let npositivo: number = document.getElementById("npositivo");
+let npositivo2: number = document.getElementById("npositivo2");
+let nnegativo: number = document.getElementById("nnegativo");
+let nnegativo2: number = document.getElementById("nnegativo2");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  let npositivo: number = Number(dato.value.npositivo);
+let npositivo2: number = Number(dato.value.npositivo);
+let nnegativo: number = Number(dato.value.npositivo);
+let nnegativo2: number = Number(dato.value.npositivo);
+while (condition) {
+  
+}
+
+}
